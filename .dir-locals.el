b9fec98a3c128af3370e9c590c53ea5371086808
Marcelo Mendez <marceloexc@tutanota.de>
2026-02-02 22:22:48 -0600

    org save -> export to html

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
-((nil . ((compile-command . "php -S localhost:9000 -t ."))))
+((nil . ((compile-command . "php -S localhost:9000 -t .")
+         (eval . (add-hook 'after-save-hook
+                           (lambda ()
+                             (when (and (derived-mode-p 'org-mode)
+                                        (eq major-mode 'org-mode))
+                               (org-html-export-to-html)))
+                           nil t)))))
